e5e4c61cb5950c58fde0b70895a114bb73e1f21d
GetsugAa4 <emmleroy811@gmail.com>
2021-09-14 09:51:55 +0200

    Added DOM Manipulation events
    Added 3 functions interacting with the DOM to change the timespan of the hours spent doing stuff.

--- Frontend Masters/1. Time Tracking Dashboard/time-tracking-dashboard-main/time-tracking-dashboard-main/script.js
+++ Frontend Masters/1. Time Tracking Dashboard/time-tracking-dashboard-main/time-tracking-dashboard-main/script.js
@@ -1,2 +1,30 @@
 const previousTime = document.getElementsByClassName('previousTime');
+const day = document.getElementsByClassName('daily');
+const week = document.getElementsByClassName('weekly');
+const month = document.getElementsByClassName('monthly');
+function displayDaily() {
+    // * Hide week and month
+    for (i = 0 ; i < week.length ; i++) {
+        week[i].style.display = 'none';
+        month[i].style.display = 'none';
+        day[i].style.display='block';
+        }
+}
 
+function displayWeekly() {
+    // * Hide day and month
+    for (i = 0 ; i < week.length ; i++) {
+        day[i].style.display = 'none';
+        month[i].style.display = 'none';
+        week[i].style.display='block';
+        }
+}
+
+function displayMonthly() {
+    // * Hide day and week
+    for (i = 0 ; i < week.length ; i++) {
+        day[i].style.display = 'none';
+        week[i].style.display = 'none';
+        month[i].style.display='block';
+        }
+}
