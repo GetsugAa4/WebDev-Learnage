3c8fb0e08b4b077b8dcad46e2a7cbe9fbcb72ef6
GetsugAa4 <emmleroy811@gmail.com>
2021-09-16 17:54:16 +0200

    Changing html
    - Applying <form> syntax
    - Styling
    - Starting the maths

---
+++ Frontend Masters/2. Tip Calculator App/script.js
@@ -0,0 +1,18 @@
+// * Reset form function
+function resetForm() {
+    document.getElementById('form').reset();
+}
+    
+// * Doing the maths
+
+/* -- NEED :
+    - input#bill
+    - [] of .tip-flex-container>input
+    - input#divideBy
+*/
+
+// ? Taking the values needed
+
+function tipAmount(bill, percentage, divide) {
+
+}
