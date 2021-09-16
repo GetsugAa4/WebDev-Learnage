6c9cb4ed2df2db221e5574fcbc907a6f1868fad0
GetsugAa4 <emmleroy811@gmail.com>
2021-09-16 18:12:22 +0200

    maths - a start
    starting the maths

--- Frontend Masters/2. Tip Calculator App/script.js
+++ Frontend Masters/2. Tip Calculator App/script.js
@@ -11,8 +11,16 @@ function resetForm() {
     - input#divideBy
 */
 
-// ? Taking the values needed
-
 function tipAmount(bill, percentage, divide) {
+    
+    // Taking the values needed
+    var bill = document.getElementById("bill").value;
+    var divide = document.getElementById("divideBy").value;
 
+    console.log(bill,percentage,divide);
+
+    // If any isn't a number, say it needs a number AND No input can be 0
+    if (bill == 0 || percentage == 0 || divide == 0 ) {
+        
+    }
 }
