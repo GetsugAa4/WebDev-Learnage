a5bc4f5ab01f6871b1c2650d8fc6fae287f871ba
GetsugAa4 <emmleroy811@gmail.com>
2021-09-17 08:30:02 +0200

    MATHS IS WORKING

--- Frontend Masters/2. Tip Calculator App/script.js
+++ Frontend Masters/2. Tip Calculator App/script.js
@@ -10,14 +10,29 @@ function resetForm() {
     - [] of .tip-flex-container>input
     - input#divideBy
 */
+// ! Getting the percentage from the buttons
+
+var percent = 0 ;
+function setPercentage(x) {
+    return percent = x ;
+}
+
 
 function tipAmount(bill, percentage, divide) {
     
     // Taking the values needed
     var bill = document.getElementById("bill").value;
     var divide = document.getElementById("divideBy").value;
+    console.log(bill,percent,divide);
 
-    console.log(bill,percentage,divide);
+    //  !  Calculus
+
+    var totalTip = (bill/100)*percent;
+    console.log(totalTip + ' Total tip');
+
+    var tipPerPerson = totalTip/divide;
+    console.log(tipPerPerson + ' Tip per person');
+
 
     // If any isn't a number, say it needs a number AND No input can be 0
     if (bill == 0 || percentage == 0 || divide == 0 ) {
